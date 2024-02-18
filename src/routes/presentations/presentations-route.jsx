3cb993fc0fee34fb2953679b4876bb1ad1424ca1
togetheruser <omr.obasi28@gmail.com>
2024-02-18 23:32:27 +0200

    revert 'PresentationsComponent'

--- src/routes/presentations/presentations-route.jsx
+++ src/routes/presentations/presentations-route.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../../components/navbar/navbar-component";
+import PresentationsComponent from "../../components/ourWork/presentations/presentations-component";
 import ContactUs from "../../components/contactUs/whiteContactUs";
 
 function Presentations(props) {
   return (
     <div>
       <Navbar />
+      <PresentationsComponent />
       <ContactUs />
     </div>
   );
